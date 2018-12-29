c8fa60b7fc3155e31ad126bf0157441f98c11d9a
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-12-29 06:57:59 -0500

    Removed old comments from code

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -9,17 +9,3 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 // This is the background script for the extension
 console.log("DMX BS!!!");
-
-// Listening for messages
-// chrome.runtime.onMessage.addListener(receiver);
-//
-// function receiver(request, sender, sendResponse) {
-//   console.log("DMX Sender BS: ", sender);
-//
-//   // Showing the page action if the content script says to
-//   if (request.message === "show_page_action") {
-//     chrome.pageAction.show(sender.tab.id, function() {
-//       chrome.tabs.sendMessage(sender.tab.id, {"url": sender.tab.url});
-//     });
-//   }
-// }
