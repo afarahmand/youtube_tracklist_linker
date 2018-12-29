4f8c6521f8864f910ffee3d67457dcca5907e99a
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-12-29 18:43:36 -0500

    Changed a table column name for consistency

--- chrome_extension/scripts/api_utils.js
+++ chrome_extension/scripts/api_utils.js
@@ -7,17 +7,17 @@ const fetchJSON = videoId => {
       status: "active",
       tracklist: {
         1: {
-          startTime: "0:00",
+          start: "0:00",
           track: "[Fibre - Supernatural](https://www.youtube.com/watch?v=JHqeZi5W05A)",
           sample: "[The Limit - Miracles](https://www.youtube.com/watch?v=Ghvyy4mCmRQ), [Tavares - Got to Find My Way Back to You](https://www.youtube.com/watch?v=Ydjai0K2lLw)"
         },
         2: {
-          startTime: "2:33",
+          start: "2:33",
           track: "[Night Tempo - Dreamer](https://www.youtube.com/watch?v=srX3csM1lhU)",
           sample: "[Minako Yoshida - Light'n Up](https://www.youtube.com/watch?v=VF1U8cdv6Dk)"
         },
         3: {
-          startTime: "5:06",
+          start: "5:06",
           track: "[Cape Coral - Lovely Slut](https://www.youtube.com/watch?v=oppcFmWxGAw)",
           sample: "[Change - Lovely Lady](https://www.youtube.com/watch?v=UpuDPdAPz2w)"
         }
@@ -30,22 +30,22 @@ const fetchJSON = videoId => {
       status: "active",
       tracklist: {
         1: {
-          startTime: "0:00",
+          start: "0:00",
           track: "[コンピュータサイバー魂﻿PC'86 - OPEN UP YOUR HEART](https://www.youtube.com/watch?v=oM0wctqMfus)",
           sample: "[BONES JONES - open up your heart 86](https://www.youtube.com/watch?v=6mPVZ5GvWgc)"
         },
         2: {
-          startTime: "3:10",
+          start: "3:10",
           track: "[Saint Pepsi - Skylar Spence](https://www.youtube.com/watch?v=AJA77S11BmM)",
           sample: "[Tatsuro Yamashita - Love Talkin' (Honey It's You)](https://www.youtube.com/watch?v=f3sU6DMzG1I)"
         },
         3: {
-          startTime: "5:14",
+          start: "5:14",
           track: "[ALPHA CENTAURI アルファ - C:\This Feelin'](https://www.youtube.com/watch?v=tqto41sT-e0)",
           sample: "[Klique - I Can't Shake This Feeling](https://www.youtube.com/watch?v=qhvnDw6zyLQ)"
         },
         16: {
-          startTime: "35:33",
+          start: "35:33",
           track: "[AnTgry - DANCE!](https://www.youtube.com/watch?v=BrhzJtfdojM)",
           sample: "[Jimmy Senyah - Weakness For Your Sweetness](https://www.youtube.com/watch?v=jJ5hFjqYaXQ)"
         }
