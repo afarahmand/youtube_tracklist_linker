8a0824cb45d991efda706c2b23e0874e7128ab6d
Ashil Farahmand <ashil.farahmand@gmail.com>
2019-01-01 15:07:59 -0500

    Removed extraneous comments

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,11 +1,9 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
-    // console.log("DMX BS tab: ", tab.url);
     chrome.pageAction.show(tab.id, function() {
       chrome.tabs.sendMessage(tab.id, {"url": tab.url});
     });
   }
 });
 
-// This is the background script for the extension
 console.log("DMX BS!!!");
